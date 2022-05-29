f2fdc0871aaa0a880ab3433ad312d3d618aeda89
stefancho <stefancho@joseongjins-MacBook-Pro.local>
2022-05-29 22:54:49 +0900

    test

--- create-pr.js
+++ create-pr.js
@@ -1,8 +1,6 @@
 module.exports = async ({ context, github }, customForFitpet) => {
   const { repo, owner } = context.repo;
   const { title, body, head } = context.payload.pull_request;
-  console.log('bbbaseeee')
-  console.dir(base);
   const result = await github.rest.pulls.create({
     title,
     owner,
@@ -15,6 +13,6 @@ module.exports = async ({ context, github }, customForFitpet) => {
     owner,
     repo,
     issue_number: result.data.number,
-    labels: [base, 'pending'],
+    labels: [customForFitpet.base, 'pending'],
   });
 };
