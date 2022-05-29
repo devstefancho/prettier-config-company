1ca0d8d2481700273485237bac83a7d8346f9c12
stefancho <stefancho@joseongjins-MacBook-Pro.local>
2022-05-29 21:37:34 +0900

    test

---
+++ .github/workflows/create-pr.js
@@ -0,0 +1,18 @@
+moudule.export = async ({ context, github, base }) => {
+  const { repo, owner } = context.repo;
+  const { title, body, head } = context.payload.pull_request;
+  const result = await github.rest.pulls.create({
+    title,
+    owner,
+    repo,
+    head: head.ref,
+    base,
+    body,
+  });
+  github.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: result.data.number,
+    labels: [base, 'pending'],
+  });
+};
