62ea24ea3b7101888c9cbbb4ab5694cb4f916c39
stefan cho <61320923+devstefancho@users.noreply.github.com>
2022-06-09 23:53:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ console.log('hello world 🤚')
 // pr testing
 console.log('hello world 🤚')
 console.log('hello world 🤚')
+
+module.exports = {
+  test: function() {
+    console.log('Hello World!')
+  }
+}
